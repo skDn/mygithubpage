bfb1933d33e8ecaeb0d13a366f357379c2fd80c3
Yordan Yordanov <2038011y@student.gla.ac.uk>
2016-10-02 13:13:11 +0100

    Added twitter posts as images. Changed the image host from facebook to postimg.

--- src/components/FacebookPosts.js
+++ src/components/FacebookPosts.js
@@ -1,38 +1,37 @@
 import React from 'react';
-import FacebookProvider, { EmbeddedPost } from 'react-facebook';
 
 var FacebookPosts = React.createClass({
   getInitialState: function () {
       return {
         neutralImagesSrc : [
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14383466_1435139019835257_648122314_n.png?oh=cf9ccffedd13a62d40a4682d00647fb0&oe=57E9A7E2",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14383515_1435136633168829_1734263586_n.png?oh=4866ded91c89fa94c8499bf3798e4fd1&oe=57E8981F",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14365377_1435135549835604_302486912_n.png?oh=81555aa976bdb923cc27186b0a636c25&oe=57E8B33F",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14389029_1435134679835691_516762672_n.png?oh=f65a6cfcbe5f1e13fc2ca7309c9bf17d&oe=57E9B3F2",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14349009_1435133909835768_1424191189_n.png?oh=0ae4ef4a0949ca79f9250d4d895f2bbc&oe=57E991EE",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14331770_1435132983169194_1071092364_n.png?oh=8bd7ca199665094d1fb60477b7357d49&oe=57E96931",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14388812_1435131836502642_1787808255_n.png?oh=18cff03fd2efe15f0a852a25dda29bf2&oe=57E98849",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14341925_1435131116502714_1438382819_n.png?oh=d14426f5d6be20f146b0eda259eeb21a&oe=57E99C35",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14349190_1435130056502820_453857713_n.png?oh=d70e26c820f11d23198c55cca4b07267&oe=57E876F1",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14383523_1435129549836204_1691774324_n.png?oh=92547c5dff95685f7a7e86dbbf5c8286&oe=57E98AE6",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14341768_1435128729836286_1493375169_n.png?oh=80b265645fcd94996f46d2bd58069c6f&oe=57E88DD0",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14383533_1435127963169696_1146826771_n.png?oh=e487137f780ab7eaeea69328b66ee6d7&oe=57E8AC73",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14348975_1435127266503099_1922792105_n.png?oh=7173c079857067dacd4a5f38f06572b9&oe=57E97F38",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14348708_1435125819836577_1177528652_n.png?oh=005f21a612eddef11b39e6f08a1ebc73&oe=57E9787C",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14348638_1435125103169982_1891154696_n.png?oh=2fbba1391d2b054b676a810fab7b9f16&oe=57E99C0B",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14348621_1435123026503523_1418370704_n.png?oh=f4b3887c33d226c23550000a65f2899f&oe=57E8741F",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14388896_1435121319837027_1685348835_n.png?oh=27e65ff63da06887f004eca78ac87249&oe=57E8821B",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14341574_1435118789837280_614241699_n.png?oh=a1d77a4ae11608cacbd1fad4bb0d4692&oe=57E9B425",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14389028_1435115403170952_1550358995_n.png?oh=7dca1056817bbdbd8e483cc7067ce1e2&oe=57E96CD2",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14348875_1435112436504582_1187516147_n.png?oh=8c6919e5e7a8d9a4e50ca86b72376492&oe=57E8A2CD"
+          "https://s14.postimg.org/49yzrs1g1/14383466_1435139019835257_648122314_n.jpg",
+          "https://s14.postimg.org/6skosgn69/14383515_1435136633168829_1734263586_n.jpg",
+          "https://s14.postimg.org/jy06ykh1t/14365377_1435135549835604_302486912_n.jpg",
+          "https://s14.postimg.org/4dst816xd/14389029_1435134679835691_516762672_n.jpg",
+          "https://s14.postimg.org/jnsof82fl/14349009_1435133909835768_1424191189_n.jpg",
+          "https://s14.postimg.org/mj5rm36fl/14331770_1435132983169194_1071092364_n.jpg",
+          "https://s14.postimg.org/fh7tzw2tt/14388812_1435131836502642_1787808255_n.jpg",
+          "https://s14.postimg.org/jrmhvh7wx/14341925_1435131116502714_1438382819_n.jpg",
+          "https://s14.postimg.org/jg51iprgx/14349190_1435130056502820_453857713_n.jpg",
+          "https://s14.postimg.org/teq0570wh/14383523_1435129549836204_1691774324_n.jpg",
+          "https://s14.postimg.org/4muzbp93l/14341768_1435128729836286_1493375169_n.jpg",
+          "https://s14.postimg.org/s2cwh1sup/14383533_1435127963169696_1146826771_n.jpg",
+          "https://s14.postimg.org/tioez6vrl/14348975_1435127266503099_1922792105_n.jpg",
+          "https://s14.postimg.org/mgqhczs5t/14348708_1435125819836577_1177528652_n.jpg",
+          "https://s14.postimg.org/g4bc35p3l/14348638_1435125103169982_1891154696_n.jpg",
+          "https://s14.postimg.org/x646593yp/14348621_1435123026503523_1418370704_n.jpg",
+          "https://s14.postimg.org/vscj9y4pd/14388896_1435121319837027_1685348835_n.jpg",
+          "https://s14.postimg.org/yay8amqfl/14341574_1435118789837280_614241699_n.jpg",
+          "https://s14.postimg.org/5mla7eo9d/14389028_1435115403170952_1550358995_n.jpg",
+          "https://s14.postimg.org/bepzsangx/14348875_1435112436504582_1187516147_n.jpg"
         ],
         positiveImagesSrc : [
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14348759_1435113893171103_526164711_n.png?oh=cda3ad25f137aa4a836bd5e2009dee57&oe=57E8B5D0",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14365327_1435089923173500_1136063769_n.png?oh=37bad8b2d4bdcbe138a4a2f366fbc2bc&oe=57E993AE"
+          "https://s14.postimg.org/rn1mo16xd/14348759_1435113893171103_526164711_n.jpg",
+          "https://s14.postimg.org/m2tqr4xg1/14365327_1435089923173500_1136063769_n.jpg"
         ],
         negativeImagesSrc : [
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14388907_1435113073171185_96059495_n.png?oh=c72915627c57eff7192e8726265f3eba&oe=57E9986D",
-          "https://scontent-lhr3-1.xx.fbcdn.net/v/t34.0-12/14365406_1435089689840190_150328757_n.png?oh=5f74c70435a2176bbbef27e96cd585c1&oe=57E89531"
+          "https://s14.postimg.org/sqqcdqgy9/14388907_1435113073171185_96059495_n.jpg",
+          "https://s14.postimg.org/bh9vf4r4h/14365406_1435089689840190_150328757_n.jpg"
         ]
       }
   },
--- src/components/TwitterPosts.js
+++ src/components/TwitterPosts.js
@@ -1,11 +1,79 @@
 import React from 'react';
-import TweetEmbed from './TweetEmbed';
 
-const TwitterPosts = React.createClass({
+var TwitterPosts = React.createClass({
+  getInitialState: function () {
+      return {
+        neutralImagesSrc : [
+          "https://s22.postimg.org/3op6h1xxd/14509418_1448223658526793_564358391_n.jpg",
+          "https://s22.postimg.org/550oz70u9/14501898_1448222201860272_1637197189_n.jpg",
+          "https://s22.postimg.org/gin8aebcx/14508478_1448221225193703_905500541_n.jpg",
+          "https://s22.postimg.org/q4gsqp2ip/14501982_1448220638527095_259090360_n.jpg",
+          "https://s22.postimg.org/j2iv4hywx/14501906_1448219928527166_562559958_n.jpg",
+          "https://s22.postimg.org/u3e09ir5t/14508515_1448218898527269_1232516005_n.jpg",
+          "https://s22.postimg.org/d451u9fy9/14518824_1448218618527297_1446924070_n.jpg",
+          "https://s22.postimg.org/8v5dp7j0h/14528477_1448218301860662_725075233_n.jpg",
+          "https://s22.postimg.org/bqigw2n0h/14518381_1448218018527357_1534005757_n.jpg",
+          "https://s22.postimg.org/djldke875/14518455_1448217311860761_1942201493_n.jpg",
+          "https://s22.postimg.org/o7p4j8i69/14527640_1448217015194124_1280768197_n.jpg",
+          "https://s22.postimg.org/665zlfo5d/14509265_1448216141860878_974437692_n.jpg",
+          "https://s22.postimg.org/555qwb75t/14508754_1448215715194254_348208937_n.jpg",
+          "https://s22.postimg.org/j041es1kx/14470822_1448215041860988_1285773455_n.jpg",
+          "https://s22.postimg.org/naipad6o1/14518570_1448214668527692_2106655533_n.jpg",
+          "https://s22.postimg.org/46pdu0ttt/14470892_1448214125194413_624431779_n.jpg",
+          "https://s22.postimg.org/o2ld9kav5/14518786_1448213688527790_1320506912_n.jpg",
+          "https://s22.postimg.org/fyhuikdtt/14509150_1448213108527848_1257778277_n.jpg"
+        ],
+        positiveImagesSrc : [
+          "https://s22.postimg.org/3l50bno5d/14483819_1448210995194726_599943025_n.jpg"
+        ],
+        negativeImagesSrc : [
+          "https://s22.postimg.org/iv4viujnl/14518290_1448210751861417_1770446361_n.jpg"
+        ]
+      }
+  },
+
+  getImagesSrc : function () {
+    var initialArray = this.state.neutralImagesSrc;
+
+    if (this.props.positive) {
+      initialArray = initialArray.concat(this.state.positiveImagesSrc)
+    }
+    else {
+      initialArray = initialArray.concat(this.state.negativeImagesSrc)
+    }
+
+    this.shuffle(initialArray);
+
+    return initialArray.map((imageSrc, index) => (
+      <div className="row" key={index}>
+        <img className="img-responsive center-block" src={imageSrc} style={{marginBottom : "10px"}}/>
+      </div>
+    ));
+    
+  },
+  /**
+  * Shuffles array in place.
+  * @param {Array} a items The array containing the items.
+  */
+  shuffle : function (a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+  },
+
   render() {
+    var array = this.getImagesSrc();
     return (
-      <TweetEmbed id="763480140357730304" />
-		);
+      <div className="row">
+        {
+          this.getImagesSrc()
+        }
+      </div>
+    );
   }
 });
 
